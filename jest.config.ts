1de68ff558c6472324523d44b56e41d07a991e5a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-01-31 20:20:50 -0300

    chore: add tests config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  bail: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/*.spec.ts'
+  ]
+}
