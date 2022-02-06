21c63411a51fef3db8ce049061d6d044cc69bd35
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-06 18:36:50 -0300

    chore: separate unit test from integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,18 @@
+import path from 'path'
+
 export default {
+  rootDir: path.resolve(__dirname),
+  displayName: 'unit:tests',
   bail: true,
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
-  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*UseCase.ts'],
+  coverageDirectory: 'coverage/unit',
   coverageProvider: 'v8',
   coverageReporters: ['text-summary', 'lcov'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '**/*.spec.ts'
+    '**/*UseCase.spec.ts'
   ]
 }
---
+++ test/jest.config.ts
@@ -0,0 +1,20 @@
+import rootConfig from '../jest.config'
+
+export default {
+  ...rootConfig,
+  displayName: 'integration:tests',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/modules/**/dtos/*.ts',
+    '!<rootDir>/src/modules/**/repositories/I*Repository.ts',
+    '!<rootDir>/src/modules/**/repositories/in-memory/*.ts',
+    '!<rootDir>/src/providers/**/dtos/*.ts',
+    '!<rootDir>/src/providers/**/in-memory/*.ts',
+    '!<rootDir>/**/*.spec.ts'
+  ],
+  coverageDirectory: 'coverage/integration',
+  testMatch: [
+    '**/*Controller.spec.ts'
+  ]
+}
