a2f2d16b996fa7d515c472dbfec726e8005a9e85
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-06 17:47:45 -0300

    chore: add config for tests

---
+++ prisma/prisma-environment-jest.js
@@ -0,0 +1,38 @@
+const NodeEnvironment = require('jest-environment-node')
+const { randomUUID } = require('crypto')
+const { execSync } = require('child_process')
+const { Client } = require('pg')
+const dotenv = require('dotenv')
+
+const prismaCli = './node_modules/.bin/prisma'
+
+dotenv.config({
+  path: '.env.test'
+})
+
+class CustomEnvironment extends NodeEnvironment {
+  constructor (config) {
+    super(config)
+    this.schema = randomUUID()
+    this.connectionString = `${process.env.DATABASE_URL}${this.schema}`
+  }
+
+  setup () {
+    process.env.DATABASE_URL = this.connectionString
+    this.global.process.env.DATABASE_URL = this.connectionString
+
+    execSync(`${prismaCli} migrate dev`)
+  }
+
+  async teardown () {
+    const client = new Client({
+      connectionString: this.connectionString
+    })
+
+    await client.connect()
+    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`)
+    await client.end()
+  }
+}
+
+module.exports = CustomEnvironment
