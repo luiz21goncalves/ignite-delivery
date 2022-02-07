97e68a198db5a877baf1541dc18541d4e424725f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-07 07:48:35 -0300

    feat: add hash provider

---
+++ src/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -0,0 +1,14 @@
+import { hash, compare } from 'bcrypt'
+import { IHashProvider } from '../dtos/IHashProvider'
+
+class BCryptHashProvider implements IHashProvider {
+  public async generateHash (payload: string): Promise<string> {
+    return hash(payload, 10)
+  }
+
+  public async compareHash (payload: string, hashed: string): Promise<boolean> {
+    return compare(payload, hashed)
+  }
+}
+
+export const bcryptHashProvider = new BCryptHashProvider()
