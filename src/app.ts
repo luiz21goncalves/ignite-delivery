923d9d7e50ee5dbb841bc746ed0fe010d50b2974
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-08 18:36:51 -0300

    docs: add docs for user create

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
 
 import { routes } from './routes'
 import { errorHandler } from './middlewares/errorHandler'
+import swaggerFile from '../swagger/v1/swagger.json'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use('/docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(errorHandler)
 
