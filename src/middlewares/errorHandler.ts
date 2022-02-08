400d54bacb6bdbf1c2ad60cd13ea033feec5c774
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-08 20:45:00 -0300

    feat: add custom validation errors

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,11 +1,36 @@
+import { isCelebrateError } from 'celebrate'
 import { NextFunction, Request, Response } from 'express'
 import { AppError } from '../errors/AppError'
+
+type ErrorMessages = {
+  field: string;
+  error: string
+}
 
 export function errorHandler (error: Error, request: Request, response: Response, next: NextFunction) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       statusCode: error.statusCode,
       message: error.message
+    })
+  }
+
+  if (isCelebrateError(error)) {
+    const messages = [] as ErrorMessages[]
+
+    for (const [, joiError] of error.details.entries()) {
+      joiError.details.forEach((detail) => {
+        messages.push({
+          error: detail.message,
+          field: String(detail.path[0])
+        })
+      })
+    }
+
+    return response.status(400).json({
+      statusCode: 400,
+      key: 'validation.failed',
+      messages
     })
   }
 
