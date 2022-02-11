945d18ffcd154fe0f5ec45c56f34b777394a775d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-10 21:25:14 -0300

    feat: add authentication field validation

---
+++ src/modules/users/useCases/authenticateUser/validation.ts
@@ -0,0 +1,11 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+
+export const validationWhenAutenticateUser = celebrate(
+  {
+    [Segments.BODY]: {
+      password: Joi.string().required(),
+      email_or_username: Joi.string().required(),
+    },
+  },
+  { abortEarly: false },
+);
