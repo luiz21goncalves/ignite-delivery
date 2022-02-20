f4a23ed5384d570a9d7415e5bd5427bdb5366132
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-02-20 13:18:33 -0300

    test(deliveries): middleware ensure authenticate

--- src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.spec.ts
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.spec.ts
@@ -5,15 +5,18 @@
 import supertest from 'supertest';
 
 import { app } from '../../../../app';
+import { AuthenticationError } from '../../../../errors/AuthenticationError';
 import { bcryptHashProvider } from '../../../../providers/HashProvider/implementations/BCryptHashProvider';
 import { Deliveryman } from '../../../deliveryman/dtos/Deliveryman';
 import { prismaDeliverymansRepository } from '../../../deliveryman/repositories/implementations/PrismaDeliverymansRepository';
 import { User } from '../../../users/dtos/User';
 import { prismaUsersRepository } from '../../../users/repositories/implementations/PrismaUsersRepository';
+import { AuthenticateUserUseCase } from '../../../users/useCases/authenticateUser/AuthenticateUserUseCase';
 import { CreateDeliveryError } from './CreateDeliveryError';
 
 let user: User;
 let deliveryman: Deliveryman;
+let user_token: string;
 
 describe('CreateDeliveryController', () => {
   beforeAll(async () => {
@@ -32,15 +35,31 @@ describe('CreateDeliveryController', () => {
       username: 'edmund.schupper',
       password,
     });
+
+    const authenticationUserUseCase = new AuthenticateUserUseCase(
+      prismaUsersRepository,
+      bcryptHashProvider,
+    );
+
+    const { token } = await authenticationUserUseCase.execute({
+      email_or_username: 'zella.langosh',
+      password: 'password',
+    });
+
+    user_token = token;
   });
 
   it('should be able o create new delivery', async () => {
-    const { body, statusCode } = await supertest(app).post('/deliveries').send({
-      item_name: 'libero est quo',
-      status: 'received',
-      user_id: user.id,
-      deliveryman_id: deliveryman.id,
-    });
+    const { body, statusCode } = await supertest(app)
+      .post('/deliveries')
+      .set({
+        Authorization: `Bearer ${user_token}`,
+      })
+      .send({
+        item_name: 'libero est quo',
+        status: 'received',
+        deliveryman_id: deliveryman.id,
+      });
 
     expect(statusCode).toEqual(201);
     expect(body).toMatchObject({
@@ -54,24 +73,57 @@ describe('CreateDeliveryController', () => {
     });
   });
 
-  it('should be able o create new delivery without a valid user', async () => {
+  it('should not be able to create new delivery without authenticated user', async () => {
     const { body, statusCode } = await supertest(app).post('/deliveries').send({
       item_name: 'libero est quo',
       status: 'received',
-      user_id: 'invalid-user-id',
     });
 
-    expect(statusCode).toEqual(400);
-    expect(body).toEqual(new CreateDeliveryError.UserNotFound());
+    expect(statusCode).toEqual(401);
+    expect(body).toEqual(new AuthenticationError.TokenMissing());
   });
 
-  it('should be able o create new delivery without a valid deliveryman', async () => {
-    const { body, statusCode } = await supertest(app).post('/deliveries').send({
-      item_name: 'libero est quo',
-      status: 'received',
-      user_id: user.id,
-      deliveryman_id: 'invalid-deliveryman-id',
-    });
+  it('should not be able to create new delivery with invalid token', async () => {
+    const { body, statusCode } = await supertest(app)
+      .post('/deliveries')
+      .set({
+        Authorization: 'Bearer invalid-token',
+      })
+      .send({
+        item_name: 'libero est quo',
+        status: 'received',
+      });
+
+    expect(statusCode).toEqual(401);
+    expect(body).toEqual(new AuthenticationError.InvalidToken());
+  });
+
+  it('should not be able to create new delivery with missing token', async () => {
+    const { body, statusCode } = await supertest(app)
+      .post('/deliveries')
+      .set({
+        Authorization: 'Bearer',
+      })
+      .send({
+        item_name: 'libero est quo',
+        status: 'received',
+      });
+
+    expect(statusCode).toEqual(401);
+    expect(body).toEqual(new AuthenticationError.TokenMissing());
+  });
+
+  it('should not be able o create new delivery without a valid deliveryman', async () => {
+    const { body, statusCode } = await supertest(app)
+      .post('/deliveries')
+      .set({
+        Authorization: `Bearer ${user_token}`,
+      })
+      .send({
+        item_name: 'libero est quo',
+        status: 'received',
+        deliveryman_id: 'invalid-deliveryman-id',
+      });
 
     expect(statusCode).toEqual(400);
     expect(body).toEqual(new CreateDeliveryError.DeliverymanNotFound());
